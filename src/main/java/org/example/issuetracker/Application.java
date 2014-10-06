cd715f2888286ab9a09ffe16333ce9d50585c93a
Matt Warman <warmanmr@gmail.com>
2014-10-06 16:36:07 -0400

    Added @Configuration and @ComponentScan.

--- src/main/java/org/example/issuetracker/Application.java
+++ src/main/java/org/example/issuetracker/Application.java
@@ -2,11 +2,15 @@ package org.example.issuetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Spring Boot main application
  */
+@Configuration
 @EnableAutoConfiguration
+@ComponentScan
 public class Application 
 {
     public static void main( String[] args ) throws Exception
