39337aad9aaee2ac7e70496bff6cf2fdb112e421
Matt Warman <warmanmr@gmail.com>
2014-10-06 16:39:29 -0400

    Added web.api package. Added IssueController.

---
+++ src/main/java/org/example/issuetracker/web/api/IssueController.java
@@ -0,0 +1,40 @@
+package org.example.issuetracker.web.api;
+
+import java.util.List;
+
+import org.example.issuetracker.model.jpa.Issue;
+import org.example.issuetracker.service.IssueService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IssueController {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private IssueService issueService;
+	
+	@RequestMapping(value="/issues", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Issue>> getAllIssues() {
+		logger.info("> getAllIssues");
+		
+		List<Issue> issues = null;
+		try {
+			issues = issueService.findAll();
+		} catch(Exception e) {
+			return new ResponseEntity<List<Issue>>(issues,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		logger.info("< getAllIssues");
+		return new ResponseEntity<List<Issue>>(issues, HttpStatus.OK);
+	}
+	
+}
