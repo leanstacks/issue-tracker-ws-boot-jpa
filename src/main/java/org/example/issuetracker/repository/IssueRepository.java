e887d2eda22167aa3255eeab156a9d5ed0dd98fa
Matt Warman <warmanmr@gmail.com>
2014-10-06 16:38:22 -0400

    Added repository package.  Added IssueRepository.

---
+++ src/main/java/org/example/issuetracker/repository/IssueRepository.java
@@ -0,0 +1,10 @@
+package org.example.issuetracker.repository;
+
+import org.example.issuetracker.model.jpa.Issue;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IssueRepository extends JpaRepository<Issue, Long> {
+
+}
