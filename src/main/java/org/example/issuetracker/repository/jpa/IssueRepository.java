4d2f63265afe7b25405d9945fd19490daba6a596
Matt Warman <warmanmr@gmail.com>
2014-10-07 12:19:06 -0400

    Moved IssueRepository to repository.jpa package.

--- src/main/java/org/example/issuetracker/repository/jpa/IssueRepository.java
+++ src/main/java/org/example/issuetracker/repository/jpa/IssueRepository.java
@@ -1,4 +1,4 @@
-package org.example.issuetracker.repository;
+package org.example.issuetracker.repository.jpa;
 
 import org.example.issuetracker.model.jpa.Issue;
 import org.springframework.data.jpa.repository.JpaRepository;
--- src/main/java/org/example/issuetracker/service/IssueServiceBean.java
+++ src/main/java/org/example/issuetracker/service/IssueServiceBean.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.example.issuetracker.model.jpa.Issue;
-import org.example.issuetracker.repository.IssueRepository;
+import org.example.issuetracker.repository.jpa.IssueRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
