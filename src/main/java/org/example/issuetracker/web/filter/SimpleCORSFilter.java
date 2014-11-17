ff5091be76caf7e2c9c81a879b112254d5698def
Matt Warman <warmanmr@gmail.com>
2014-11-17 06:39:25 -0500

    Added PUT method to CORS Filter.

--- src/main/java/org/example/issuetracker/web/filter/SimpleCORSFilter.java
+++ src/main/java/org/example/issuetracker/web/filter/SimpleCORSFilter.java
@@ -26,7 +26,7 @@ public class SimpleCORSFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods",
-                "POST, GET, OPTIONS, DELETE");
+                "DELETE, GET, OPTIONS, PATCH, POST, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
                 "x-requested-with, content-type");
